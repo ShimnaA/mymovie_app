74ed5b70c4091e0eef89c5b2775a4c947a925002
shimna <shimnaa.arangil@gmail.com>
2020-07-12 14:25:20 +0530

    search/list movies

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Movie
 
-# Create your views here.
+def home_page(request):
+    user_query = str(request.GET.get('query', ''))
+    search_result = Movie.objects.filter(name__icontains=user_query)
+    stuff_for_frontend = {'search_result': search_result}
+    return render(request, 'movies/movies_stuff.html', stuff_for_frontend)
